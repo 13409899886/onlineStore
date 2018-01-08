9f771c878e84a60ced2cb0477cf08584cabf633b
13409899886 <331069442@qq.com>
2018-01-08 17:34:38 +0800

    。。。

--- onlineStore/src/main.js
+++ onlineStore/src/main.js
@@ -1,13 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import HelloWorld from './components/HelloWorld'
+
+import Vuex from "vuex"
+Vue.use(Vuex)
+
+import VueRouter from "vue-router"
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  template: '<HelloWorld/>',
+  components: { HelloWorld }
 })
